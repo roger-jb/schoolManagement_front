07be3364860e2029598e7cf635eccbf51affdbfe
Soow <sonia.taisse@gmail.com>
2016-11-17 17:31:14 +0100

    [STA] Design + Bouton deconnection + Code js deconnection/rester connecter

--- app/controlers/MainCtrl.js
+++ app/controlers/MainCtrl.js
@@ -29,7 +29,7 @@
         .config(function($mdThemingProvider) {
 
             $mdThemingProvider.theme('default')
-                .primaryPalette('pink', {
+                .primaryPalette('indigo', {
                     'default': '400', // by default use shade 400 from the pink palette for primary intentions
                     'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
                     'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
